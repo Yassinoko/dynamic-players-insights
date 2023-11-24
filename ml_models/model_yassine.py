d84de1adf727dc71d39ef8cc4dcc8730723e9795
Yassine Bouaine <bouaineyas@gmail.com>
2023-11-24 17:20:57 +0900

    New structure, final one

--- ml_models/model_yassine.py
+++ ml_models/model_yassine.py
@@ -6,41 +6,44 @@ from keras.layers import Dense, GlobalAveragePooling2D
 from keras.models import Model
 
 # Importing data functions
-from ml_models.data_library.get_images import get_players_images_cropped
-from ml_models.data_library.image_preprocessing import processing_images
-from ml_models.data_library.class_preprocessing import preprocessing_classes
+from data_library.data_processing import black_white, crop, delete_face_images, encode
 
 # Importing preprocessing functions
 from sklearn.model_selection import train_test_split
 
+directory_path =  "/Users/yassinebouaine/code/Yassinoko/dynamic-players-insights/data_processing/raw_data/faces"
 
-# Saving the model in a variable
-resnet = ResNet50V2(weights='imagenet', include_top=False)
+faces_folder = "../data_processing/raw_data/faces/"
+cropped_faces_directory = "../data_processing/raw_data/cropped_faces"
 
-# Processing the data
-img_label_dict = get_players_images_cropped()
-X, y = processing_images(img_label_dict)
-y_cat = preprocessing_classes(y)
+# # Processing the data
+# black_white(faces_folder)
+# crop(faces_folder, cropped_faces_directory)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y_cat, test_size=0.2)
+# delete_face_images(cropped_faces_directory)
 
+labels, players = encode(cropped_faces_directory)
 
-# Add a new classification head
-layer = resnet.output
-layer = GlobalAveragePooling2D()(layer)
-predictions = Dense(y.shape[0], activation='softmax')(layer)
+# X_train, X_test, y_train, y_test = train_test_split(X, y_cat, test_size=0.2)
 
-# Create a new model with the added classification head
-model = Model(inputs=resnet.input, outputs=predictions)
+# # Saving the model in a variable
+# resnet = ResNet50V2(weights='imagenet', include_top=False)
 
+# # Add a new classification head
+# layer = resnet.output
+# layer = GlobalAveragePooling2D()(layer)
+# predictions = Dense(y.shape[0], activation='softmax')(layer)
 
-# Compile the model
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+# # Create a new model with the added classification head
+# model = Model(inputs=resnet.input, outputs=predictions)
 
-# Train the model
-model.fit(X_train,
-          y_train,
-          epochs=10,
-          validation_split=0.2)
+# # Compile the model
+# model.compile(optimizer='adam',
+#               loss='categorical_crossentropy',
+#               metrics=['accuracy'])
+
+# # Train the model
+# model.fit(X_train,
+#           y_train,
+#           epochs=10,
+#           validation_split=0.2)
